8af43755a3aebda05caabf2e63dae40bf51a44d6
ricky-blip <ricky.rinaldy77@gmail.com>
2023-01-27 20:00:04 +0700

    update

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Ricky Rinaldy | Flutter Dev',
+      title: 'Ricky Rinaldy | Flutter Developer',
       theme: ThemeData(
         primarySwatch: Colors.blue,
         elevatedButtonTheme: elevatedButtonThemeData,
